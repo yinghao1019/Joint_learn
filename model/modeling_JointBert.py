065a60d63e127acb3d00c787c33a8a2a0942c2b0
Ying Hao <F108157110@nkust.edu.tw>
2021-02-07 07:26:56 +0800

    build model module,package
    build jointBert class,intent_classifier
    and slot_classifier

--- model/modeling_JointBert.py
+++ model/modeling_JointBert.py
@@ -1,60 +1,73 @@
 import torch
 import torch.nn as nn
-from transformers.modeling_bert import BertPreTrainedModel,BertModel
-from .module import IntentClassifier,SlotClassifier
+from torch.nn import functional as F
+from transformers import BertModel, BertPreTrainedModel
 from torchcrf import CRF
+from .module import intent_classifier, slot_classifier
+
+
 class JointBert(BertPreTrainedModel):
-    def __init__(self,config,num_intent_labels,num_slot_labels,args):
-        super(JointBert,self).__init__(config)
-        self.args=args
-        config.return_dict=True
-        self.bert=BertModel(config=config)#load pretrain Model
-        self.num_intent_labels=num_intent_labels
-        self.num_slot_labels=num_slot_labels
-        self.intent_classifier=IntentClassifier(config.hidden_size,self.num_intent_labels,self.args.dropout_rate)
-        self.slot_classifier=SlotClassifier(config.hidden_size,self.num_slot_labels,self.args.dropout_rate)
-        #determined crf
+    def __init__(self, config, args, intent_num_labels, slot_num_labels, dropout_rate):
+        super(JointBert, self).__init__(config)
+        self.intent_num_labels = intent_num_labels
+        self.slot_num_labels = slot_num_labels
+        self.args = args
+        self.config = config
+
+        # create layer
+        self.bert = BertModel(config)
+        self.intent_classifier = intent_classifier(self.config.hidden_size,
+                                                   intent_num_labels, dropout_rate)
+        self.slot_classifier = slot_classifier(self.config.hiddeb_size,
+                                               slot_num_labels, dropout_rate)
+        # build conditional random field
         if args.use_crf:
-            self.crf=CRF(num_tags=num_slot_labels,batch_first=True)
-    def forward(self,input_ids,attention_mask,token_type_ids,intent_label_ids,slot_label_ids,return_dict=True):
-        return_dict=return_dict if return_dict is not None else self.config.use_return_dict
-        output=self.bert(input_ids=input_ids,
-                        attention_mask=attention_mask,
-                        token_type_ids=token_type_ids,
-                        )
-        #fetch model last hidden state
-        sequence_output=output[0]
-        cls_output=output[1]
+            self.crf_layer = CRF(slot_num_labels, batch_first=True)
 
-        intent_logitics=self.intent_classifier(cls_output)
-        slots_logitics=self.slot_classifier(sequence_output)
-        total_loss=0
-        #1.compute intent loss
-        if intent_label_ids is not None:
-            if self.num_intent_labels==1:
-                loss_fct=nn.BCEWithLogitsLoss()
-                intent_loss=loss_fct(intent_logitics.view(-1),intent_label_ids.view(-1))
+        self.init_weights()
+
+    def forward(self, input_ids, attention_mask, token_type_ids, slot_labels, intent_labels):
+        # get bert hidden state
+        outputs = self.bert(
+            input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids)
+
+        hidden_output = outputs[0]  # sequence of hiddn state
+        pooled_output = outputs[1]  # cls token hidden state
+
+        # intent_logitics=[Bs,intent_num_labels]
+        # slot_logitics=[Bs,max_seqLen,slot_num_labels]
+        slot_logitics = self.slot_classifier(hidden_output)
+        intent_logticis = self.intent_classifier(pooled_output)
+
+        total_loss = 0
+        # compute
+        if intent_labels is not None:
+            if self.intent_num_labels == 1:
+                intent_loss = F.mse_loss(intent_logticis, intent_labels)
             else:
-                loss_fct=nn.CrossEntropyLoss()
-                intent_loss=loss_fct(intent_logitics.view(-1,self.num_intent_labels),intent_label_ids.view(-1))
-            total_loss+=intent_loss
-        #2.compute slot loss
-        if slot_label_ids is not None:
+                intent_loss = F.cross_entropy(intent_logticis, intent_labels)
+            total_loss += intent_loss
+
+        if slot_labels is not None:
             if self.args.use_crf:
-                slot_loss=self.crf(slots_logitics,slot_label_ids,mask=attention_mask.byte(),reduction='mean')
-                slot_loss=-1*slot_loss#negative log likelihood
+                slot_loss = self.crf_layer(slot_logitics, slot_labels,
+                                           mask=attention_mask.byte(), reduction='mean')
+                slot_loss = -1*slot_loss  # negative log likelihood
             else:
-                loss_fct=nn.CrossEntropyLoss()
                 if attention_mask is not None:
-                    active_loss=attention_mask.view(-1)==1#filter should compute loss position 
-                    active_logitics=slots_logitics.view(-1,self.num_slot_labels)[active_loss]#fetch not pad logitics
-                    active_slot_labels=slot_label_ids.view(-1)[active_loss]
-                    slot_loss=loss_fct(active_logitics,active_slot_labels)
+                    active_mask = attention_mask.view(-1) == 1
+                    active_logictics = slot_logitics.view(
+                        -1, self.slot_num_labels)[active_mask]
+                    active_labels = slot_labels.view(-1)[active_mask]
+                    slot_loss = F.cross_entropy(
+                        active_logictics, active_labels)
                 else:
-                    slot_loss=loss_fct(slots_logitics.view(-1,self.num_slot_labels),slot_label_ids.view(-1))
-            total_loss+=self.args.slot_loss_coef*slot_loss
-        outputs=((intent_logitics,slots_logitics),)+output[2:] #add hidden state & attention
-        outputs=(total_loss,)+outputs
-        return outputs # return total_loss,logitics,hidden_states,attentions
+                    slot_loss = F.cross_entropy(
+                        slot_logitics.view(-1, self.slot_num_labels), slot_labels.view(-1))
+            total_loss += self.args.slot_loss_coef*slot_loss
 
+        outputs = ((intent_logticis, slot_logitics),)+outputs[2:]
+        outputs = (total_loss,)+outputs
 
+        # return total_loss,logitics,hidden_state,attention, #logticis contain intent_logitics,slot_logiticis
+        return outputs
--- model/module.py
+++ model/module.py
@@ -1,17 +1,29 @@
 import torch.nn as nn
-class IntentClassifier(nn.Module):
-    def __init__(self,input_dim,output_dim,dropout_r=0.1):
-        super(IntentClassifier,self).__init__()
-        self.linear_layer=nn.Linear(input_dim,output_dim)
-        self.dropout=nn.Dropout(dropout_r)
-    def forward(self,input_features):
-        x=self.dropout(input_features)
-        return self.linear_layer(x)
-class SlotClassifier(nn.Module):
-    def __init__(self,input_dim,output_dim,dropout_r=0.1):
-        super(SlotClassifier,self).__init__()
-        self.Linear=nn.Linear(input_dim,output_dim)
-        self.dropout=nn.Dropout(dropout_r)
-    def forward(self,inputs_feature):
-        x=self.dropout(inputs_feature)
-        return self.Linear(x)
+
+
+class intent_classifier(nn.Module):
+    def __init__(self, input_dim, output_dim, dropout_rate):
+        super(intent_classifier, self).__init__()
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.fc_layer = nn.Linear(input_dim, output_dim)
+        self.dropout = dropout_rate
+
+    def forward(self, hiddens):
+        # forward pass
+        hiddens = self.dropout(hiddens)
+        return self.fc_layer(hiddens)
+
+
+class slot_classifier(nn.Module):
+    def __init__(self, input_dim, output_dim, dropout_rate):
+        super(slot_classifier, self).__init__()
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.fc_layer = nn.Linear(input_dim, output_dim)
+        self.dropout = dropout_rate
+
+    def forward(self, hiddens):
+        # forward pass
+        hiddens = self.dropout(hiddens)
+        return self.fc_layer(hiddens)
