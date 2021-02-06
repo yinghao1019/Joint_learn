394c664fc1f42f6f04d23e54689508d2d53045da
Ying Hao <F108157110@nkust.edu.tw>
2021-02-07 07:19:32 +0800

    Insert func in utils.py & delete test.py
    Insert slot,inten,sents_frame_acc metrics
    and logger config,random seed.Delete test.py

--- test.py
+++
@@ -1,4 +0,0 @@
-import logging
-logging.basicConfig(filename='test.log',
-                    format='%(asctime)s:%(levelname)s:%(message)s', datefmt='%d/%m/%Y', level=logging.INFO)
-logging.info('hello world')
--- utils.py
+++ utils.py
@@ -1,88 +1,84 @@
 import os
 import logging
 import random
+import numpy as np
 
-from transformers import BertConfig,BertTokenizer
+from transformers import BertConfig, BertTokenizer
 from model import modeling_JointBert
-MODEL_CLASSES={
-    'joint_bert':(BertConfig,BertTokenizer,modeling_JointBert.BertModel)
+from seqeval.metrics import f1_score, recall_score, precision_score
+MODEL_CLASSES = {
+    'joint_bert': (BertConfig, BertTokenizer, modeling_JointBert.JointBert)
 }
-MODEL_PATH={
-    'joint_bert':'bert-base-uncased',
+MODEL_PATH = {
+    'joint_bert': 'bert-base-uncased',
 }
 
 
 def get_intent_labels(args):
-    return [label.strip() for label in open(os.path.join(args.data_dir,args.task,'intent_label.txt','r',encoding='utf-8'))]
+    return [label.strip() for label in open(os.path.join(args.data_dir, args.task, 'intent_label.txt', 'r', encoding='utf-8'))]
+
+
 def get_slot_labels(args):
-    return [label.strip() for label in open(os.path.join(args.data_dir,args.task,'slot_label.txt','r',encoding='utf-8'))]
+    return [label.strip() for label in open(os.path.join(args.data_dir, args.task, 'slot_label.txt', 'r', encoding='utf-8'))]
+
 
 def load_tokenizer(args):
-    return MODEL_Classes[args.model_type][1].from_pretrained(MODEL_PATH[args.model_type])
+    return MODEL_CLASSES[args.model_type][1].from_pretrained(MODEL_PATH[args.model_type])
 
 
-def get_slot_metrics(predict_slots, slot_labels):
-    assert len(predict_slots) == len(slot_labels)
+def get_intent_metrics(preds, labels):
+    '''get accuracy for sents intent'''
+    assert len(preds) == len(labels)
+    return {'intent_acc': (preds == labels).mean(), }
+
+
+def get_slot_metrics(preds, labels):
+    '''According IOB1 scheme to evaluate seq'''
     return {
-        'slot_precision': precision_score(slot_labels, predict_slots),
-        'slot_recall': recall_score(slot_labels, predict_slots),
-        'slot_f1_score': recall_score(slot_labels, predict_slots),
+        'slot_f1': f1_score(labels, preds),
+        'slot_recall': recall_score(labels, preds),
+        'slot_precision': precision_score(labels, preds),
     }
 
 
-def get_intent_metrics(predict_intent, intent_labels):
-    acc = np.where(predict_intent == intent_labels, 1, 0).mean()
+def get_sents_frame_acc(slot_preds, intent_preds, slot_labels, intent_labels):
+    # 1.get intent acc
+    intent_correct = intent_preds == intent_labels
+    # 2.compute sents acc
+    # confirm same shape
+    assert slot_preds.shape[0] == slot_labels.shape[0]
+    assert slot_preds.shape[1] == slot_labels.shape[1]
+
+    slot_correct = np.all(slot_preds == slot_labels, axis=1)
+    semantic_acc = np.multiply(intent_correct, slot_correct).mean()
+
     return {
-        'intent_accuracy': acc,
+        'semantic_frame_acc': semantic_acc,
     }
 
 
-def set_seed(args):
-    random.seed(args.seed)
-    np.random.seed(args.seed)
-    torch.manual_seed(args.seed)
-    if args.use_gpu and torch.cuda.is_available():
-        torch.cuda.manual_seed_all(args.seed)
+def get_modelMetrics(slot_preds, intent_preds, slot_labels, intent_labels):
+    assert len(slot_preds) == len(intent_preds) == len(
+        slot_labels) == len(intent_labels)
+    metrics = {}
+    metrics.update(get_intent_metrics(intent_preds, intent_labels))
+    metrics.update(get_slot_metrics(slot_preds, slot_labels))
+    metrics.update(get_sents_frame_acc(
+        slot_preds, intent_preds, slot_labels, intent_labels))
+    return metrics
+# set random seed
 
 
-def get_sentence_frame_acc(predict_intents, predict_slots, intent_labels, slot_labels):
-    '''for this case that  intent and all slots are correct in one sentence'''
-    # 1.get the intent comparsion result
-    intent_result = predict_intents == intent_labels
-    # 2.get the slot comparsion result
-    slot_result = []
-    for preds, slots in zip(predict_slots, slot_labels):
-        assert len(preds) == len(slots)
-        one_sentence_correct = True
-        for p, l in zip(preds, slots):
-            if p != l:
-                one_sentence_correct = False
-                break
-        slot_result.append(one_sentence_correct)
-    slot_result = np.array(slot_result)
-    semantic_result = np.multiply(intent_result, slot_result)
-    return {
-        'semantic_acc': semantic_result.mean(),
-    }
+def set_randomSeed(args):
+    random.seed(args.random_seed)
+    np.random.seed(args.random_seed)
+    torch.random.manual_seed(args.random_seed)
+    torch.cuda.manual_seed_all(args.random_seed)
 
-
-def get_model_metrics(predict_intent, predict_slots, intent_labels, slot_labels):
-    assert len(predict_intent) == len(predict_slots) == len(
-        intent_labels) == len(slot_labels)
-    result = {}
-    # get each metric dict
-    intent_result = get_intent_metrics(predict_intent, intent_labels)
-    slot_result = get_slot_metrics(predict_slots, slot_labels)
-    semantic_result = get_sentence_frame_acc(
-        predict_intent, predict_slots, intent_labels, slot_labels)
-    # update current metrics
-    result.update(intent_result)
-    result.update(slot_result)
-    result.update(semantic_result)
-    return result
-# 設定要儲存log的log file
+# set logger logging message
 
 
 def init_logger():
-    logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
-                        datefmt=r'%m/%d/%Y %H:%M:%S', level=logging.INFO)
+    logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s - %(message)s',
+                        datefmt=r'%m/%d/%Y %H:%M:%S',
+                        level=logging.INFO)
